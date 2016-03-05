2a115041c8a7240520547c42be68a1be62d98201
Nilley <nilley@hotmail.com>
2016-03-05 20:40:22 +0100

    First Version

---
+++ run_analysis.R
@@ -0,0 +1,72 @@
+# Step1. Merges the training and the test sets to create one data set.
+setwd("C:/Program Files/R/Work_Coursera/MyGithub/UCI HAR Dataset")
+Data_train <- read.table("./train/X_train.txt")
+dim(Data_train) # 7352*561
+head(Data_train)
+Label_train <- read.table("./train/y_train.txt")
+table(Label_train)
+Subject_train <- read.table("./train/subject_train.txt")
+Data_test <- read.table("./test/X_test.txt")
+dim(Data_test) # 2947*561
+Label_test <- read.table("./test/y_test.txt") 
+table(Label_test) 
+Subject_test <- read.table("./test/subject_test.txt")
+Data_join <- rbind(Data_train, Data_test)
+dim(Data_join) # 10299*561
+Label_join <- rbind(Label_train, Label_test)
+dim(Label_join) # 10299*1
+Subject_join <- rbind(Subject_train, Subject_test)
+dim(Subject_join) # 10299*1
+
+# Step2. Extracts only the measurements on the mean and 
+# standard deviation for each measurement. 
+Data_features <- read.table("./features.txt")
+dim(Data_features)  # 561*2
+Indices_meanStd <- grep("mean\\(\\)|std\\(\\)", Data_features[, 2])
+length(Indices_meanStd) # 66
+Data_join <- Data_join[, Indices_meanStd]
+dim(Data_join) # 10299*66
+names(Data_join) <- gsub("\\(\\)", "", Data_features[Indices_meanStd, 2]) # remove "()"
+names(Data_join) <- gsub("mean", "Mean", names(Data_join)) # capitalize M
+names(Data_join) <- gsub("std", "Std", names(Data_join)) # capitalize S
+names(Data_join) <- gsub("-", "", names(Data_join)) # remove "-" in column names 
+
+# Step3. Uses descriptive activity names to name the activities in the data set
+Data_activity <- read.table("./activity_labels.txt")
+Data_activity[, 2] <- tolower(gsub("_", "", Data_activity[, 2]))
+substr(Data_activity[2, 2], 8, 8) <- toupper(substr(Data_activity[2, 2], 8, 8))
+substr(Data_activity[3, 2], 8, 8) <- toupper(substr(Data_activity[3, 2], 8, 8))
+Label_activity <- Data_activity[Label_join[, 1], 2]
+Label_join[, 1] <- Label_activity
+names(Label_join) <- "activity"
+
+# Step4. Appropriately labels the data set with descriptive activity names. 
+names(Subject_join) <- "subject"
+Data_cleaned <- cbind(Subject_join, Label_join, Data_join)
+dim(Data_cleaned) # 10299*68
+write.table(Data_cleaned, "First_dataset.txt") # write out the 1st dataset
+
+# Step5. From the data set in step 4, creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
+Len_subject <- length(table(Subject_join)) # 30L
+Len_activity <- dim(Data_activity)[1] # 6L
+Len_column <- dim(Data_cleaned)[2] # 68L
+Data_result <- matrix(NA, nrow=Len_subject*Len_activity, ncol=Len_column) 
+Data_result <- as.data.frame(Data_result) # 180 obs of 68 variables
+colnames(Data_result) <- colnames(Data_cleaned)
+row <- 1
+for(i in 1:Len_subject) {
+    for(j in 1:Len_activity) {
+        Data_result[row, 1] <- sort(unique(Subject_join)[, 1])[i]
+        Data_result[row, 2] <- Data_activity[j, 2]
+        bool1 <- i == Data_cleaned$subject
+        bool2 <- Data_activity[j, 2] == Data_cleaned$Data_activity
+        Data_result[row, 3:Len_column] <- colMeans(Data_cleaned[bool1&bool2, 3:Len_column])
+        row <- row + 1
+    }
+}
+head(Data_result)
+write.table(Data_result, "Second_dataset.txt") # write out the 2nd dataset
+
+#test <- read.table("./Second_dataset.txt")
+#test[1:12, 1:3]
